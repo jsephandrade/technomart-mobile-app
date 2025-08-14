57755966a920cd3eb1324ca45052e72644d315e3
Andrade, Joseph <josephformentera2@gmail.com>
2025-08-14 21:17:20 +0800

    globa.css added in app.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { TailwindProvider } from 'nativewind';
+// import { TailwindProvider } from 'nativewind';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/RootNavigator';
 import './global.css'; // Ensure global styles are imported
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </TailwindProvider>
+    // <TailwindProvider>
+    //   <NavigationContainer>
+    //     <RootNavigator />
+    //   </NavigationContainer>
+    // </TailwindProvider>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
