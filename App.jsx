9952bd248811634203b82d150274477f7b52935a
Andrade, Joseph <josephformentera2@gmail.com>
2025-08-14 19:59:20 +0800

    For testing only, do not revert

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TailwindProvider } from 'nativewind';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/RootNavigator';
+import './global.css'; // Ensure global styles are imported
 
 export default function App() {
   return (
