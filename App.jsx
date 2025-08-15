1a2a13d768514b3c7267225593543a5969f2223a
Andrade, Joseph <josephformentera2@gmail.com>
2025-08-15 20:30:11 +0800

    GestureHandlerRootView

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 // import { TailwindProvider } from 'nativewind';
 import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/RootNavigator';
 import './global.css'; // Ensure global styles are imported
 
@@ -11,8 +13,11 @@ export default function App() {
     //     <RootNavigator />
     //   </NavigationContainer>
     // </TailwindProvider>
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
